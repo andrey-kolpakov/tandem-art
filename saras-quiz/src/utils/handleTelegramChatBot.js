f920f92f720d6f3ee8cf592f04acebe17e1f4b89
Andrew <gfstels@yandex.ru>
2025-02-13 18:19:54 +0500

    firstCommit

--- saras-quiz/src/utils/handleTelegramChatBot.js
+++
@@ -1,7 +0,0 @@
-const chatID = process.env.REACT_APP_CHAT_ID
-const botToken = process.env.REACT_APP_BOT_TOKEN
-
-export default function handleTelegramChatBot(nameState, phoneState){
-
-    const message = `<b>Новая заявка от</b> ${nameState}%0A<b>Номер телефона:</b> ${phoneState}`
-}
--- saras-quiz/src/utils/importAll.js
+++
@@ -1,9 +0,0 @@
-export default function importAll(r) {
-    // console.log(r)
-
-    let images = {}
-    r.keys().map((item, index) => {
-        images[item.replace('./', '')] = r(item)
-    })
-    return images
-}
