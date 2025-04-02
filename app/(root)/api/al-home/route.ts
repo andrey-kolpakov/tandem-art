efce1ba1555a2e79fe8223cbdd99fec30a8936eb
Andrew <gfstels@yandex.ru>
2025-04-02 14:06:27 +0500

    new

--- app/(root)/api/al-home/route.ts
+++ app/(root)/api/al-home/route.ts
@@ -1,7 +1,16 @@
 import { prisma } from '@/prisma/prisma-client'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET() {
+const SECRET_KEY = process.env.ESP_SECRET_KEY // храним в .env, конечно
+
+export async function GET(req: NextRequest) {
+    const data = await req.json()
+
+    const providedSecret = data.secret
+    if (providedSecret !== SECRET_KEY) {
+        return NextResponse.json({ error: 'Forbidden' }, { status: 403 })
+    }
+
     const currentIp = await prisma.currentIp.findFirst()
 
     return NextResponse.json(currentIp)
@@ -9,6 +18,11 @@ export async function GET() {
 
 export async function POST(req: NextRequest){
     const data = await req.json()
+
+    const providedSecret = data.secret
+    if (providedSecret !== SECRET_KEY) {
+        return NextResponse.json({ error: 'Forbidden' }, { status: 403 })
+    }
 
     // Получение IP
     const forwardedFor = req.headers.get('x-forwarded-for')
