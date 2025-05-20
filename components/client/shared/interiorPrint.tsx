8c9c4faf50dcf2a29b9f0d80da7edadb83da3daf
Andrey <gfstels@yandex.ru>
2025-05-20 17:33:32 +0500

    new

--- components/client/shared/interiorPrint.tsx
+++ components/client/shared/interiorPrint.tsx
@@ -18,66 +18,19 @@ export const InteriorPrint: React.FC<Props> = ({className}) => {
             header: "Печать на холсте",
             image: "/images/interior-print/1.jpg",
             text: <div>
-                <p>Интерьерная печать фотографий, постеров, репродукций и других изображений на холсте с натяжкой на подрамник</p>
-                <br/>
-                <strong>На холсте можно напечатать картину, имитирующую масляную живопись, семейную фотографию, модульные картины, постеры, плакаты и афиши</strong>
+                <p>Интерьерная печать фотографий, репродукций на холсте, с натяжкой на подрамник</p>
             </div>,
             link: "/canvas-printing",
             newTab: true,
         },
         {
-            header: "Печать на обоях",
+            header: "Таблички, стенды и указатели",
             image: "/images/interior-print/2.jpg",
             text: <div>
-                <p>Печать на обоях — это возможность сделать интерьер неповторимым, отвечающий именно вашему вкусу.</p>
-                <br/>
-                <p className={'tp-bold'}>С помощью фотообоев можно создать пространство с эксклюзивным дизайном, обновить интерьер без больших затрат, скорректировать недостатки помещения и неровности стен.</p>
-                <br/>
-                <p>У нас вы можете заказать изготовление фотообоев с любым сюжетом, орнаментом, рисунком или даже с фотографией из семейного архива.</p>
-            </div>,
-            link: "/background-printing",
-            newTab: false,
-            popupContent:
-                <div className={'interior-print__popup-form'}>
-                    <Image src={'/images/interior-print/2.jpg'} width={200} height={300} alt={"Печать на холсте"}/>
+                <p>Основа ПВХ 5-10мм</p>
+                <p>Фотопечать 1440 dpi</p>
+                <p>Крепление и кайма на выбор</p>
 
-                    <div className={'interior-print__popup-form-text'}>
-                        <h2>Печать на обоях</h2>
-                        <p>Печать на обоях – это возможность сделать интерьер неповторимым, отвечающим именно вашему вкусу.</p>
-
-                        <p>С помощью фотообоев можно создать пространство с эксклюзивным дизайном, обновить интерьер без больших затрат, скорректировать недостатки помещения и неровности стен.</p>
-
-                        <p>У нас вы можете заказать изготовление фотообоев с любым сюжетом, орнаментом, рисунком или даже с фотографией из семейного архива.</p>
-
-                        <Form submitAction={submitForm}/>
-                    </div>
-                </div>
-        },
-        {
-            header: "Печать плакатов",
-            image: "/images/interior-print/3.jpg",
-            text: <div>
-                <p>Плакаты — один из самых популярных и недорогих рекламных носителей.</p>
-                <br/>
-                <p className={'tp-bold'}>С их помощью можно проинформировать об акции или о появлении новой продукции, анонсировать предстоящее мероприятие, украсить интерьер магазина или офиса.</p>
-                <br/>
-                <p>У нас вы можете заказать изготовление фотообоев с любым сюжетом, орнаментом, рисунком или даже с фотографией из семейного архива.</p>
-            </div>,
-            link: "/billboard-printing",
-            newTab: true,
-        },
-        {
-            header: "Таблички и стенды",
-            image: "/images/interior-print/4.jpg",
-            text: <div>
-                               <p><strong>Изготавливаем таблички и стенды различного вида:</strong></p>
-
-                <p>- таблички для офиса<br/>
-                    - адресные таблички<br/>
-                    - навигаторы<br/>
-                    - информационные стенды<br/>
-                    - рекламные стенды и таблички<br/>
-                    - пожарной безопасности</p>
 
                 <p>и другие</p>
             </div>,
@@ -85,7 +38,7 @@ export const InteriorPrint: React.FC<Props> = ({className}) => {
             newTab: false,
             popupContent:
                 <div className={'interior-print__popup-form'}>
-                    <Image src={'/images/interior-print/4.jpg'} width={200} height={200} alt={"Печать на холсте"}/>
+                    <Image src={'/images/interior-print/2.jpg'} width={200} height={200} alt={"Печать на холсте"}/>
 
                     <div className={'interior-print__popup-form-text'} >
                         <h2>Таблички и стенды</h2>
@@ -102,7 +55,42 @@ export const InteriorPrint: React.FC<Props> = ({className}) => {
                         <Form submitAction={submitForm}/>
                     </div>
                 </div>
-        }
+        },
+        {
+            header: "Постеры, плакаты и афиши",
+            image: "/images/interior-print/3.jpg",
+            text: <div>
+                <p>Плакаты — один из самых популярных и недорогих рекламных носителей.</p>
+            </div>,
+            link: "/billboard-printing",
+            newTab: true,
+        },
+        {
+            header: "Фотопечать на обоях",
+            image: "/images/interior-print/4.jpg",
+            text: <div>
+                <p>Печать на обоях — это возможность сделать интерьер неповторимым, отвечающий именно вашему вкусу.</p>
+            </div>,
+            link: "/background-printing",
+            newTab: false,
+            popupContent:
+                <div className={'interior-print__popup-form'}>
+                    <Image src={'/images/interior-print/4.jpg'} width={200} height={300} alt={"Печать на холсте"}/>
+
+                    <div className={'interior-print__popup-form-text'}>
+                        <h2>Печать на обоях</h2>
+                        <p>Печать на обоях – это возможность сделать интерьер неповторимым, отвечающим именно вашему вкусу.</p>
+
+                        <p>С помощью фотообоев можно создать пространство с эксклюзивным дизайном, обновить интерьер без больших затрат, скорректировать недостатки помещения и неровности стен.</p>
+
+                        <p>У нас вы можете заказать изготовление фотообоев с любым сюжетом, орнаментом, рисунком или даже с фотографией из семейного архива.</p>
+
+                        <Form submitAction={submitForm}/>
+                    </div>
+                </div>
+        },
+
+
     ];
 
     return (
@@ -111,7 +99,7 @@ export const InteriorPrint: React.FC<Props> = ({className}) => {
 
             <br/>
 
-            <div className={'vertical-cards-container vertical-cards-container--horizontal'}>
+            <div className={'vertical-cards-container '}>
                 {cards.map((el, index) => (
                     <VerticalCard key={`vertical-card__${index}`} cardInfo={el}/>
                 ))}
